0385e2d221adffde771afbeb44bf0cf9ef232b80
Albertus Chua <albertus.chua@gmail.com>
2022-04-28 12:03:24 -0400

    Coding challenge for 4/28/22

---
+++ CW 4-28-22 welcome/main.js
@@ -0,0 +1,22 @@
+function greet(language) {
+	let greeting = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  return greeting[language] || greeting.english;
+}
