51e91c14883d7e7683beb22d812b200a232450c7
Albertus Chua <albertus.chua@gmail.com>
2022-07-28 14:42:49 -0400

    Coding challenge for 7/28/22

---
+++ 6 kyu/CW 7-28-22 sort the odd/main.js
@@ -0,0 +1,28 @@
+// Task
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// **Solution**
+
+function sortArray(array) {
+    // Filter array holding odd numbers only
+    // Sort that odd number array
+    // Loop through original array and check if the number is odd
+    // If the number is odd, splice out the number and add [0] value from odd number array. Then .shift the odd number array.
+    // Return final result
+  
+    const oddNums = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+  
+    for (i = 0; i < array.length; i++) {
+      if (array[i] % 2 !== 0) {
+        array.splice(i, 1, oddNums[0]);
+        oddNums.shift();
+      }
+    }
+    return array;
+    
+}
