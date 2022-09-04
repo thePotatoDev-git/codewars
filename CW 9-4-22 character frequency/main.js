6f81551a83c6e7e007fd05d7fe1ec5343fcf831d
Albertus Chua <albertus.chua@gmail.com>
2022-09-04 12:48:51 -0400

    Coding challenge for 9/4/22

---
+++ CW 9-4-22 character frequency/main.js
@@ -0,0 +1,24 @@
+// Description
+// Welcome, Warrior! In this kata, you will get a message and you will need to get the frequency of each and every character!
+
+// Explanation
+// Your function will be called char_freq/charFreq/CharFreq and you will get passed a string, you will then return a dictionary (object in JavaScript) with as keys the characters, and as values how many times that character is in the string. You can assume you will be given valid input.
+
+// Example
+// charFreq("I like cats") // Returns {'a': 1, ' ': 2, 'c': 1, 'e': 1, 'I': 1, 'k': 1, 'l': 1, 'i': 1, 's': 1, 't': 1}
+
+// **Solution**
+
+function charFreq(message) {
+    // Split message into an array
+    // Sort the array
+    // Count duplicates and store in an object with for..of
+    
+    message = message.split('').sort();
+    const counts = {};
+    
+    for (const letter of message) {
+      counts[letter] = counts[letter] ? counts[letter] + 1 : 1;
+    }
+    return counts;
+}
