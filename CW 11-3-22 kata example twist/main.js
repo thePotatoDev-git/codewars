12508fe256a9e27663824081672949bea83094e4
Albertus Chua <albertus.chua@gmail.com>
2022-11-03 12:39:58 -0400

    Coding challenge for 11/3/22

---
+++ CW 11-3-22 kata example twist/main.js
@@ -0,0 +1,14 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// **Solution**
+
+// Loop to check if the length of websites is less than 1000
+// Push 'codewars' if it is
+
+var websites = []
+
+while (websites.length < 1000) {
+  websites.push('codewars')
+}
