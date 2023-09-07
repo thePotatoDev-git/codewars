b671ce1dac542d7a012bb0271c0d0f3ac187b6b5
Albertus Chua <albertus.chua@gmail.com>
2023-09-07 16:26:08 -0400

    Coding challenge for 9/7/23

---
+++ 6 kyu/CW 9-7-23 follow that spy/main.js
@@ -0,0 +1,52 @@
+// We are diligently pursuing our elusive operative, Matthew Knight, who also goes by the alias Roy Miller. He employs a nomadic lifestyle to evade detection, constantly moving from one location to another, with each of his journeys following a perplexing and non-standard sequence of itineraries. Our mission is to decipher the routes he will undertake during each of his voyages.
+
+// Task
+// You've been provided with an array of itinerary routes, decipher the precise destinations he will visit in the correct sequence according to his meticulously planned itineraries.
+
+// Example
+// Based on the provided routes:
+
+// [ [USA, BRA], [JPN, PHL], [BRA, UAE], [UAE, JPN] ]
+// The correct sequence of destinations is:
+
+// "USA, BRA, UAE, JPN, PHL"
+// Note:
+
+// You can safely assume that there will be no duplicate locations with distinct routes.
+// All routes provided will have non-empty itineraries.
+// There will always be at least one (1) route connecting one waypoint to another.
+
+// **Solution**
+
+function findRoutes(routes) {
+    // Find the first route by finding which city doesn't have a second occurrence
+    // Find the currentRoute by filtering routes to include the last city, but not the previous city
+    // Keep looping until currentRoute is empty
+  
+    // Find first route
+    const routesJoined = routes.join(' ');
+    let start = '';
+  
+    for (let i = 0; i < routes.length; i++) {
+      if (routesJoined.indexOf(routes[i][0]) === routesJoined.lastIndexOf(routes[i][0])) {
+        start = routes[i][0];
+      }
+    }
+  
+    const destinations = routes.filter(arr => arr.includes(start))[0];
+    let end = false;
+    
+    while (end === false) {
+      let currentRoute = routes.filter(arr => arr.includes(destinations[destinations.length - 1]) && !arr.includes(destinations[destinations.length - 2]))
+  
+      if (currentRoute.length > 0) {
+        destinations.push(currentRoute[0][1]);
+      } else {
+        end = true;
+      }
+      
+    }
+    
+    return destinations.join(', ');
+    
+}
