09095898bf98f1fd7354a246b27643dd2c5834de
Albertus Chua <albertus.chua@gmail.com>
2023-11-14 17:29:26 -0500

    Coding challenge for 11/14/23

---
+++ CW 11-14-23 (SQL) register for the party/main.sql
@@ -0,0 +1,19 @@
+-- You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+
+-- participants table schema
+
+-- name (string)
+-- age (integer)
+-- attending (boolean)
+-- NOTES:
+
+-- Since alcohol will be served, you can only attend if you are 21 or older
+-- You can't attend if the attending column returns anything but true
+-- NOTE: Your solution should use pure SQL. Ruby is used within the test cases just to validate your answer.
+
+-- **Solution**
+
+INSERT INTO participants (name, age, attending) -- Insert into participants table in name, age, attending columns
+VALUES ('Albert', 21, true); -- Values to insert
+
+SELECT * FROM participants;
