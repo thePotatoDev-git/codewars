156ee13664e02c280a5f1479c38b629e509472d4
Albertus Chua <albertus.chua@gmail.com>
2023-12-15 19:44:52 -0500

    Coding challenge for 12/15/23

---
+++ CW 12-15-23 is there a vowel in there/main.js
@@ -0,0 +1,28 @@
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// Return the resulting array.
+
+// **Solution**
+
+function isVow(a){
+    // Loop through a
+    // If a[i] is a code for a vowel, replace the number with the vowel
+    
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === 97) {
+        a[i] = 'a';
+      } else if (a[i] === 101) {
+        a[i] = 'e';
+      } else if (a[i] === 105) {
+        a[i] = 'i';
+      } else if (a[i] === 111) {
+        a[i] = 'o';
+      } else if (a[i] === 117) {
+        a[i] = 'u';
+      }
+    }
+    
+    return a;
+}
