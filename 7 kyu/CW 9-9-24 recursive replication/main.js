eab829282f03bfb2852c1364baf40e008614a593
Albertus Chua <albertus.chua@gmail.com>
2024-09-09 08:28:48 -0700

    Coding challenge for 9/9/24

---
+++ 7 kyu/CW 9-9-24 recursive replication/main.js
@@ -0,0 +1,17 @@
+// You need to design a recursive function called replicate which will receive arguments times and number.
+
+// The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+
+// As tempting as it may seem, do not use loops to solve this problem.
+
+// **Solution**
+
+function replicate(times, number) {
+    // Base case
+    if (times <= 0) {
+      return [];
+    } else {
+      // Concat existing array with replicated array
+      return [number].concat(replicate(times - 1, number));
+    }
+}
