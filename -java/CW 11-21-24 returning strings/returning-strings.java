62551b71820d616895615abc6f880d945868116b
Albertus Chua <albertus.chua@gmail.com>
2024-11-21 11:34:09 -0800

    Coding challenge for 11/21/24

---
+++ -java/CW 11-21-24 returning strings/returning-strings.java
@@ -0,0 +1,13 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// **Solution**
+
+public class Kata
+{
+  public static String greet(String name)
+  {
+    return "Hello, " + name + " how are you doing today?";
+  }
+}
